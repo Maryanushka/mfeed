082d2731bdc2cbfbbdaef99a344c38f9c7daa7b6
mariia.onuschuk <marya.vadymovna@gmail.com>
2023-08-16 15:11:44 +0300

    prettify

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
 		MatFormFieldModule,
 		ReactiveFormsModule,
 		MatButtonModule,
-		ReactiveFormsModule
+		ReactiveFormsModule,
 	],
 	declarations: [
 		RegisterComponent,
